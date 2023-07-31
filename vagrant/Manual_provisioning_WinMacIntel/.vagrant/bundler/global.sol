c01d13d0a912a6463bb9159b87c1d8da09ddd7d3
sanidhyasharma <sanidhyasharma9701@gmail.com>
2023-08-01 00:58:00 +0530

    https://github.com/hkhcoder/vprofile-project.git

---
+++ vagrant/Manual_provisioning_WinMacIntel/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["rubyhacks",[">= 0.1.0"]],["hostmanager",["= 1.2.2"]],["vagrant-hostmanager",["= 1.8.10"]]],"checksum":"8f4f567483141d2a60ebabc8f0b0630af2364ac457f6996ac4e77826d6201abc","vagrant_version":"2.3.7"}
